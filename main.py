e2fbc1ba33c23ba97dfaabd03c33357a08b654ee
Jefferson Marques <jeffersonm13@gmail.com>
2024-12-27 18:43:26 -0300

    Inserido o campo para preechimento da escola

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
   
 # Entrada dos dados pessoais
 nome = str(input("Digite o seu nome completo: "))
-curso = input ("Digite seu curso: ")
+curso = input("Digite seu curso: ")
 disciplina = input("Digite a disciplina que você estudou: ")
+escola = input("Digite o nome da escola: ")
 idade = int(input("Digite sua idade: "))
+
+
 
 # Entrada das notas do aluno
 nota1 = float (input("Digite sua primeira nota: "))
@@ -17,6 +20,7 @@ aprove = "Reprovado(a)" if media < 6 else "Aprovado(a)"
 print(f"Seu nome é: {nome}.")
 print(f"Você esta matriculado no curso: {curso}.") 
 print(f"Disciplina realizada: {disciplina}.") 
+print(f"A escola que você estou é: {escola}.")
 print(f"Sua média é: {media:.2f}.") 
 print(f"Você está {aprove}.") 
 
