87de099764009927bbd52249d7643009a14d8bb1
Jefferson Marques <jeffersonm13@gmail.com>
2024-12-27 18:33:40 -0300

    Inserido os comentários nos códigos

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
   
-
+# Entrada dos dados pessoais
 nome = str(input("Digite o seu nome completo: "))
 curso = input ("Digite seu curso: ")
 disciplina = input("Digite a disciplina que você estudou: ")
 idade = int(input("Digite sua idade: "))
 
+# Entrada das notas do aluno
 nota1 = float (input("Digite sua primeira nota: "))
 nota2 = float (input("Digite sua segunda nota: "))
 
+# Calculo da média das notas do aluno e aprovação
 media = ( nota1 + nota2 ) / 2 
 aprove = "Reprovado(a)" if media < 6 else "Aprovado(a)"
 
+# Saída formatada apenas se não houver erros
 print(f"Seu nome é: {nome}.")
 print(f"Você esta matriculado no curso: {curso}.") 
 print(f"Disciplina realizada: {disciplina}.") 
