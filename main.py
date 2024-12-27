c13a35c2c9a3c5c2676314078e94e5518fecdf5e
Jefferson Marques <jeffersonm13@gmail.com>
2024-12-27 17:48:31 -0300

    bug corrigido

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ nota1 = float (input  ("Digite sua primeira nota: "))
 nota2 = float (input  ("Digite sua segunda nota: "))
 media = ( nota1 + nota2 ) / 2 
 aprove = "Reprovado" if media < 6 else "Aprovado"
-print(f"Seu nome é {nome} \nVocê esta matriculado no curso {curso} \nSua média é {media:.2f}. \nVocê está {aprove}." ) 
+print(f"Seu nome é: {nome}. \nVocê esta matriculado no curso: {curso}. \nSua média é: {media:.2f}. \nVocê está {aprove}." ) 
