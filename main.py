cf62870a6956e613af9948c1e50342ed0d39ca03
Jefferson Marques <jeffersonm13@gmail.com>
2024-12-27 18:10:31 -0300

    Formatando no modo correto

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-nome = input("Digite o seu nome completo: ")
-curso = input ("Digite seu curso: ")
-idade = input("Digite sua idade: ")
-nota1 = float (input  ("Digite sua primeira nota: "))
-nota2 = float (input  ("Digite sua segunda nota: "))
-media = ( nota1 + nota2 ) / 2 
-aprove = "Reprovado" if media < 6 else "Aprovado"
-print(f"Seu nome é: {nome}. \nVocê esta matriculado no curso: {curso}. \nSua média é: {media:.2f}. \nVocê está {aprove}." ) 
+    
+    nome = str (input("Digite o seu nome completo: "))
+    curso = input ("Digite seu curso: ")
+    disciplina = input("Digite a disciplina que você estudou: ")
+    idade = int (input("Digite sua idade: "))
+
+    nota1 = float (input  ("Digite sua primeira nota: "))
+    nota2 = float (input  ("Digite sua segunda nota: "))
+
+    media = ( nota1 + nota2 ) / 2 
+    aprove = "Reprovado(a)" if media < 6 else "Aprovado(a)"
+
+print(f"
+      Seu nome é: {nome}. 
+      Você esta matriculado no curso: {curso}. 
+      Disciplina realizada: {disciplina}. 
+      Sua média é: {media:.2f}. 
+      Você está {aprove}." ) 
+
+
+
