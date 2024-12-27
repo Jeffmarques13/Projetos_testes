b60503db8b06b211658cca9d0157f73ffdc21750
Jefferson Marques <jeffersonm13@gmail.com>
2024-12-27 15:15:35 -0300

    inserido campo curso

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-nome = input("Digite o seu nome: ")
+nome = input("Digite o seu nome completo: ")
+curso = input ("Digite seu curso: ")
 idade = input("Digite sua idade: ")
 nota1 = float (input  ("Digite sua primeira nota: "))
-nota2 = float (input  ("Digite sua segunda nota2: "))
+nota2 = float (input  ("Digite sua segunda nota: "))
 media = ( nota1 + nota2 ) / 2 
 aprove = "Reprovado" if media < 6 else "Aprovado"
-print(f"Seu nome é {nome}, \nSua média é {media:.2f}. \nVocê está {aprove}." ) 
+print(f"Seu nome é {nome} \nVocê esta matriculado no curso {curso} \nSua média é {media:.2f}. \nVocê está {aprove}." ) 
